6078585d43453083227501b085d6be2afe5539f4
MaksymZhyhan <142611411+MaksymZhyhan@users.noreply.github.com>
2023-10-02 21:07:49 +0100

    Update lesson7.py

--- lesson7.py
+++ lesson7.py
@@ -1,58 +1,44 @@
 # Task1
 
-def favorite_movie():
-    movie = input("What is your favorite movie?: ")
-    print("My favorite movie is named" " " + '{' + movie + '}')
+def favorite_movie(movie):
+    print(f"My favorite movie is named {movie}")
 
 
-favorite_movie()
+favorite_movie("The Lord of the Rings")
 
 
 # Task2
 
-def make_country():
-    country_name = input("Write a country: ")
-    capital = (input("Write the capital of" " " + country_name + ":" " "))
-    your_input = {country_name: capital}
+def make_country(name, capital):
+    country = {
+        'name': name,
+        'capital': capital
+    }
+    return country
 
-    return your_input
 
-
-dictionar = make_country()
-print(dictionar)
+country_dict = make_country('France', 'Paris')
+print(country_dict)
 
 
 # Task3
 
-def make_operation():
-    return choice
+def make_operation(operator, *args):
+    result = args[0] if args else 0
+
+    if operator == '+':
+        for num in args[1:]:
+            result += num
+    elif operator == '-':
+        for num in args[1:]:
+            result -= num
+    elif operator == '*':
+        for num in args[1:]:
+            result *= num
+
+    return result
 
 
-while True:
-    print("Choose the operation: ")
-    print("Addition - 1")
-    print("Subtraction - 2")
-    print("Multiplication - 3")
-    print("Exit - 4")
-
-    choice = input("Enter the number of operation: ")
-    if choice == "4":
-        print("GoodBye")
-        break
-    if choice not in "1 , 2, 3, 4":
-        print("Wrong choice")
-        continue
-    a = float(input("Write a first number: "))
-    b = float(input("Write a second number: "))
-    c = float(input("Write a third number: "))
-
-    if choice == "1":
-        print("Result: ", a + b + c)
-    elif choice == "2":
-        print("Result: ", (a - b - c))
-    elif choice == "3":
-        print("Result: ", (a * b * c))
-    break
-
-
-make_operation()
+print(make_operation('+', 7, 7, 2))
+print(make_operation('-', 5, 5, -10, -20))
+print(make_operation('*', 7, 6))
