c46bfe7a503be2de4912d6a9bee630e4dea0c98e
MaksymZhyhan <142611411+MaksymZhyhan@users.noreply.github.com>
2023-12-05 20:13:58 +0000

    Update lesson17.py 3 task

--- lesson17.py
+++ lesson17.py
@@ -1,3 +1,6 @@
+import math
+
+
 # Task 1
 
 class Animal:
@@ -80,61 +83,118 @@ class Library:
 
 
 class Fraction:
-    def __init__(self, numerator: int, denominator: int):
-        if type(numerator) == int and type(denominator) == int:
-            self.numerator = numerator
-            self.denominator = denominator
-        else:
-            raise ValueError('Args must be integer')
+    """Create object that includes arithmetic methods for fractions (+, -, /, *)"""
 
-    def __str__(self):
-        print(f"{self.numerator}/{self.denominator}")
-        return f"{self.numerator}/{self.denominator}"
-
-    def __add__(self, other):
-        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
-        new_denominator = self.denominator * other.denominator
-        new_numerator, new_denominator = self.optimized(new_numerator, new_denominator)
-        return Fraction(new_numerator, new_denominator)
-
-    def __eq__(self, other):
-        new_numerator = self.numerator * other.denominator
-        new_denominator = other.numerator * self.denominator
-        return new_numerator == new_denominator
-
-    def __sub__(self, other):
-        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
-        new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
-
-    def __mul__(self, other):
-        new_numerator = self.numerator * other.numerator
-        new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
-
-    def __truediv__(self, other):
-        if other.numerator == 0:
-            raise ValueError("Ділення на нуль неможливе")
-        new_numerator = self.numerator * other.denominator
-        new_denominator = self.denominator * other.numerator
-        return Fraction(new_numerator, new_denominator)
+    def __init__(self, numerator, denominator):
+        if not isinstance(numerator, int) or not isinstance(denominator, int):
+            raise ValueError(
+                "Type of numerator, denominator of fraction must be integer!"
+            )
+        self.numerator = numerator
+        self.denominator = denominator
 
     @staticmethod
-    def optimized(numerator, denominator):
-        new_numerator = numerator
-        new_denominator = denominator
-        flag = True
-        while flag:
-            if not new_denominator % 2 and not new_numerator % 2:
-                new_numerator = new_numerator / 2
-                new_denominator = new_denominator / 2
-            else:
-                flag = False
-        return int(new_numerator), int(new_denominator)
+    def reduction(numerator, denominator):
+        """Return numerator, denominator after fractional reduction"""
+        gcd = math.gcd(numerator, denominator)
+        return int(numerator / gcd), int(denominator / gcd)
+
+    def __add__(self, summand):
+        if not isinstance(summand, Fraction) and not isinstance(summand, int):
+            raise ValueError("Wrong type of second argument!")
+        if isinstance(summand, int):
+            summand = Fraction(summand, 1)
+
+        denominator = math.lcm(self.denominator, summand.denominator)
+        numerator = int(
+            (self.numerator * denominator / self.denominator)
+            + (summand.numerator * denominator / summand.denominator)
+        )
+        numerator, denominator = Fraction.reduction(numerator, denominator)
+        return Fraction(numerator, denominator)
+
+    def __sub__(self, subtracted):
+        if not isinstance(subtracted, Fraction) and not isinstance(subtracted, int):
+            raise ValueError("Wrong type of second argument!")
+        if isinstance(subtracted, int):
+            subtracted = Fraction(subtracted, 1)
+
+        denominator = math.lcm(self.denominator, subtracted.denominator)
+        numerator = int(
+            (self.numerator * denominator / self.denominator)
+            - (subtracted.numerator * denominator / subtracted.denominator)
+        )
+
+        numerator, denominator = Fraction.reduction(numerator, denominator)
+        return Fraction(numerator, denominator)
+
+    def __mul__(self, multiplier):
+        if not isinstance(multiplier, Fraction) and not isinstance(multiplier, int):
+            raise ValueError("Wrong type of second argument!")
+        if isinstance(multiplier, int):
+            multiplier = Fraction(multiplier, 1)
+
+        numerator = self.numerator * multiplier.numerator
+        denominator = self.denominator * multiplier.denominator
+
+        numerator, denominator = Fraction.reduction(numerator, denominator)
+        return Fraction(numerator, denominator)
+
+    def __truediv__(self, divisor):
+        if not isinstance(divisor, Fraction) and not isinstance(divisor, int):
+            raise ValueError("Wrong type of second argument!")
+        if isinstance(divisor, int):
+            divisor = Fraction(divisor, 1)
+
+        numerator = self.numerator * divisor.denominator
+        denominator = self.denominator * divisor.numerator
+
+        numerator, denominator = Fraction.reduction(numerator, denominator)
+        return Fraction(numerator, denominator)
+
+    def __eq__(self, other):
+        if not isinstance(other, Fraction) and not isinstance(other, int):
+            raise ValueError("Wrong type of second argument!")
+        if isinstance(other, int):
+            other = Fraction(other, 1)
+        numerator_1, denominator_1 = Fraction.reduction(
+            self.numerator, self.denominator
+        )
+        numerator_2, denominator_2 = Fraction.reduction(
+            other.numerator, other.denominator
+        )
+        return numerator_1 == numerator_2 and denominator_1 == denominator_2
+
+    def is_bigger(self, other):
+        """Return True if self fraction bigger then other"""
+        denominator = math.lcm(self.denominator, other.denominator)
+        numerator_1 = self.numerator * denominator / self.denominator
+        numerator_2 = other.numerator * denominator / other.denominator
+        return numerator_1 > numerator_2
+
+    def __str__(self):
+        return f"Fraction({self.numerator}, {self.denominator})"
 
 
-x = Fraction(1, 2)
-y = Fraction(1, 4)
-print(x + y)
-print(Fraction(3, 4))
-assert x + y == Fraction(3, 4)
+if __name__ == "__main__":
+    x = Fraction(1, 2)
+    y = Fraction(1, 4)
+    print("1/2 + 1/4 =", x + y)  # 1/2 + 1/4 == Fraction(3, 4)
+    print("1/2 + 3 = ", x + 3)  # 1/2 + 3 == Fraction(7, 2)
+
+    print("1/2 - 1/4 =", x - y)  # 1/2 - 1/4 == Fraction(1, 4)
+    print("1/2 - 2 =", x - 2)  # 1/2 - 2 == Fraction(-3, 2)
+
+    print("1/2 * 1/4 =", x * y)  # 1/2 * 1/4 == Fraction(1, 8)
+    print("1/2 * 2 =", x * 3)  # 1/2 * 2 == Fraction(3, 2)
+
+    print("1/4 / 1/2 =", y / x)  # 1/4 / 1/2 == Fraction(1, 2)
+    print("1/2 / 3 =", x / 3)  # = Fraction(1, 6)
+
+    print("Is 2/4 equal 1/2?: ", Fraction(2, 4) == Fraction(1, 2))  # True
+    print("Is 4/2 equal 2?: ", Fraction(4, 2) == 2)  # True
+    print("Is 2 equal 4/2?: ", 2 == Fraction(4, 2))  # True
+    print("Is 3/2 equal 1/2?: ", Fraction(3, 2) == Fraction(1, 2))  # True
+
+    print("Is 1/2 bigger then 1/4?: ", x.is_bigger(y))  # True
+    print("Is 1/4 bigger then 1/2?: ", y.is_bigger(x))  # False
